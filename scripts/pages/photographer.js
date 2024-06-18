f57760c80c32545c16531495cbc4537fd272dd13
Stéphane Bazzé <zarakeye@users.noreply.github.com>
2024-06-18 23:57:40 +0200

    JS not inline in photographer.html anymore

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,8 @@
 //Mettre le code JavaScript lié à la page photographer.html
+import { displayModal, closeModal } from "../utils/contactForm.js";
+
+const contactButton = document.querySelector('.contact_button');
+const closeFormBtn = document.querySelector('.modal header img');
+
+contactButton.addEventListener('click', displayModal);
+closeFormBtn.addEventListener('click', closeModal);
