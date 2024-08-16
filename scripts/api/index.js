dd9abe9edfeb41bba0adbc49049a987b4fa67af6
Stéphane Bazzé <zarakeye@users.noreply.github.com>
2024-08-16 22:43:05 +0200

    api/index.js improved

--- scripts/api/index.js
+++ scripts/api/index.js
@@ -1,27 +1,20 @@
 export const Api = {
-  datas: {
-    getDatas: async function () {
-      try {
-        const response = await fetch('../../data/photographers.json');
-        const datas = await response.json();
-        return datas;
-      } catch (error) {
-        console.error('an error occured when fetching datas : ', error);
-      }
-    }
-  },
-
   photographers: {
     getPhotographers : async function() {
       try {
-        return await Api.datas.getDatas().then((datas) => datas.photographers);
+        return await fetch('../../data/photographers.json')
+          .then((response) => response.json())
+          .then((datas) => datas.photographers);
       } catch (error) {
         console.error('an error occured when fetching photographers datas : ', error);
       }
     },
     getPhotographerById : async function(photographerId) {
       try {
-        return await Api.photographers.getPhotographers().then((photographers) => photographers.find((photographer) => photographer.id === photographerId));
+        return await fetch('../../data/photographers.json')
+          .then((response) => response.json())
+          .then((datas) => datas.photographers)
+          .then((photographers) => photographers.find((photographer) => photographer.id === photographerId));
       } catch (error) {
         console.error('an error occured when fetching photographer datas : ', error);
       }
@@ -31,14 +24,18 @@ export const Api = {
   medias: {
     getMedias : async function () {
         try {
-          return await Api.datas.getDatas().then((datas) => datas.media);
+          return await fetch('../../data/photographers.json')
+            .then((response) => response.json())
+            .then((datas) => datas.media);
         } catch (error) {
             console.error('an error occured when fetching medias datas : ', error);
         }
     },
     getMediasByPhotographerId : async function(photographerId) {
       try {
-        return await Api.medias.getMedias().then((medias) => medias.filter((media) => media.photographerId === photographerId));
+        return await fetch('../../data/photographers.json')
+        .then((response) => response.json())
+        .then((datas) => datas.media).then((medias) => medias.filter((media) => media.photographerId === photographerId));
       } catch (error) {
         console.error('an error occured when fetching medias datas : ', error);
       }
